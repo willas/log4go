61e5ab340b547c33aa6ab43deaeeaa85f5c320ff
willas <fei265274@163.com>
2017-05-02 17:45:08 +0800

    更改文件权限，更改rotate 文件数

--- filelog.go
+++ filelog.go
@@ -66,7 +66,7 @@ func NewFileLogWriter(fname string, rotate bool) *FileLogWriter {
 		filename:  fname,
 		format:    "[%D %T] [%L] (%S) %M",
 		rotate:    rotate,
-		maxbackup: 2,
+		maxbackup: 6,
 	}
 
 	// open the file for the first time
